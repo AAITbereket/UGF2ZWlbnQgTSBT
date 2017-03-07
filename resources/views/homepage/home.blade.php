bf324d8b729499ec045c4053228e74a01a1e5b57
Bereket <bereketgebredingle@gmail.com>
2017-03-07 07:34:38 +0300

    homepage view is added

---
+++ resources/views/homepage/home.blade.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+<meta charset="utf-8"/>
+<title>Metronic | Page Layouts - Blank Page</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+
+	<!--materialize ui-->
+<link href="materialize/css/materialize.css" rel="stylesheet" type="text/css"/>
+
+	<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
+<link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+<link href="css/simple-line-icons.min.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/uniform.default.css" rel="stylesheet" type="text/css">
+<!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN THEME STYLES -->
+<link href="css/components.css" id="style_components" rel="stylesheet" type="text/css"/>
+<link href="css/plugins.css" rel="stylesheet" type="text/css"/>
+<link href="css/layout.css" rel="stylesheet" type="text/css"/>
+<link id="style_color" href="css/grey.css" rel="stylesheet" type="text/css"/>
+<link href="css/custom.css" rel="stylesheet" type="text/css"/>
+
+<!-- END THEME STYLES -->
+<link rel="shortcut icon" href="favicon.ico"/>
+</head>
+
+<body class="page-boxed page-header-fixed page-container-bg-solid page-sidebar-closed-hide-logo">
+<!-- BEGIN HEADER -->
+<div class="page-header navbar navbar-fixed-top">
+	<!-- BEGIN HEADER INNER -->
+	<div class="page-header-inner container">
+		<!-- BEGIN LOGO -->
+		<div class="page-logo">
+			<a href="index.html">
+			<img src="img/logo-default.png" alt="logo" class="logo-default"/>
+			</a>
+			<div class="menu-toggler sidebar-toggler">
+				<!-- DOC: Remove the above "hide" to enable the sidebar toggler button on header -->
+			</div>
+		</div>
+		<!-- END LOGO -->
+		<!-- BEGIN RESPONSIVE MENU TOGGLER -->
+		
+		<!-- END RESPONSIVE MENU TOGGLER -->
+		<!-- BEGIN PAGE ACTIONS -->
+		<!-- DOC: Remove "hide" class to enable the page header actions -->
+
+		<!-- END PAGE ACTIONS -->
+		<!-- BEGIN PAGE TOP -->
+		<div class="page-top ">
+			<!-- BEGIN HEADER SEARCH BOX -->
+			<!-- DOC: Apply "search-form-expanded" right after the "search-form" class to have half expanded search box -->
+
+			<!-- END HEADER SEARCH BOX -->
+			<!-- BEGIN TOP NAVIGATION MENU -->
+			<div class="top-menu">
+				<ul class="nav navbar-nav pull-right">
+					<!-- BEGIN NOTIFICATION DROPDOWN -->
+					<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
+					
+					<!-- END NOTIFICATION DROPDOWN -->
+					
+					<!-- BEGIN INBOX DROPDOWN -->
+					
+					<!-- END INBOX DROPDOWN -->
+					
+					<!-- BEGIN TODO DROPDOWN -->
+					
+					<!-- END TODO DROPDOWN -->
+					
+					<!-- BEGIN USER LOGIN DROPDOWN -->
+					<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
+					<li class="dropdown dropdown-user">
+						<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+						<img alt="" class="img-circle" src="img/avatar3_small.jpg"/>
+						<span class="username username-hide-on-mobile">
+						Nick </span>
+						<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="dropdown-menu dropdown-menu-default">
+							<li>
+								<a href="extra_profile.html">
+								<i class="icon-user"></i> My Profile </a>
+							</li>
+							
+							<li>
+								<a href="login.html">
+								<i class="icon-key"></i> Log Out </a>
+							</li>
+						</ul>
+					</li>
+					
+					<!-- END USER LOGIN DROPDOWN -->
+				</ul>
+			</div>
+			<!-- END TOP NAVIGATION MENU -->
+		</div>
+		<!-- END PAGE TOP -->
+	</div>
+	<!-- END HEADER INNER -->
+</div>
+<!-- END HEADER -->
+<div class="clearfix">
+</div>
+<div class="container">
+	<!-- BEGIN CONTAINER -->
+	<div class="page-container">
+		<!-- BEGIN SIDEBAR -->
+		<div class="page-sidebar-wrapper">
+			<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+			<!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+			<div class="page-sidebar navbar-collapse">
+				<!-- BEGIN SIDEBAR MENU -->
+				<!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
+				<!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
+				<!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
+				<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+				<!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
+				<!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+				<ul class="page-sidebar-menu page-sidebar-menu-hover-submenu " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+					<li class="start ">
+						<a href="index.html">
+						<i class="icon-home"></i>
+						<span class="title">Dashboard</span>
+						</a>
+					</li>
+
+					<li class="start ">
+						<a href="index.html">
+							<i class="icon-home"></i>
+							<span class="title">Dashboard</span>
+						</a>
+					</li>
+				</ul>
+				<!-- END SIDEBAR MENU -->
+			</div>
+		</div>
+		<!-- END SIDEBAR -->
+		<!-- BEGIN CONTENT -->
+		<div class="page-content-wrapper">
+			<div class="page-content">
+				<h3 class="page-title">
+				</h3>
+				<div class="page-bar">
+				</div>
+				<!-- END PAGE HEADER-->
+				<!-- BEGIN PAGE CONTENT-->
+				<div class="row">
+					<div class="col-md-12" style="text-align: center">
+						 <!--<h1> Welcome to Addis Ababa Roads Pavement Management System </h1>-->
+						<h1> Lorem ipsum dolor simet el </h1>
+					</div>
+				</div>
+				<div class="row">
+					<br/><br/>
+					<div class="col-md-12" >
+						<div class="col-md-3"></div>
+						<div class="col-md-2">
+							<button class="btn waves-effect waves-light" type="submit" name="action"> <i class="fa-plus-circle"></i> Start a project </button>
+						</div>
+						<div class="col-md-1"></div>
+						<div class="col-md-5">
+							<button class="btn waves-effect waves-light grey lighten-2" type="submit" name="action">Read Guide </button>
+						</div>
+					</div>
+					<br/><br/><br/><br/>
+				</div>
+				<hr/>
+				<div id="recent projects">
+					<div class="row">
+						<blockquote class="col-md-4">
+							Existing projects
+						</blockquote>
+
+						<div class="col-md-3">
+
+						</div>
+
+						<!--<div class="col-md-4">-->
+							<!--<form action="#">-->
+								<!--<div class="file-field input-field">-->
+									<!--<div class="btn">-->
+										<!--<span>File</span>-->
+										<!--<input type="file">-->
+									<!--</div>-->
+									<!--<div class="file-path-wrapper">-->
+										<!--<input class="file-path validate" type="text" placeholder="Import from file">-->
+									<!--</div>-->
+								<!--</div>-->
+							<!--</form>-->
+
+						<!--</div>-->
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<table class="table container" border="4">
+								<thead class="thead-default">
+									<tr class="row">
+										<td> <h4> 2 projects </h4> </td>
+									</tr>
+								</thead>
+
+								<tr class="row">
+									<td class="col-md-4"> Megengna to Ayat </td>
+									<td class="col-md-3"> Sub title </td>
+									<td class="col-md-5">
+										<li class="dropdown dropdown-user" style="list-style: none;">
+											<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+												<i class="fa fa-angle-down"></i>
+											</a>
+											<ul class="dropdown-menu dropdown-menu-default">
+												<li>
+													<a href="extra_profile.html">
+														<i class="icon-user"></i> Edit </a>
+												</li>
+
+												<li>
+													<a href="login.html">
+														<i class="icon-key"></i> Delete </a>
+												</li>
+											</ul>
+										</li>
+									</td>
+								</tr>
+
+								<tr class="row">
+									<td class="col-md-4"> 6kilo to Goro </td>
+									<td class="col-md-6"> title </td>
+									<td class="col-md-2">
+										<li class="dropdown dropdown-user" style="list-style: none;">
+											<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+												<i class="fa fa-angle-down"></i>
+											</a>
+											<ul class="dropdown-menu dropdown-menu-default">
+												<li>
+													<a href="extra_profile.html">
+														<i class="icon-user"></i> Edit </a>
+												</li>
+
+												<li>
+													<a href="login.html">
+														<i class="icon-key"></i> Delete </a>
+												</li>
+											</ul>
+										</li>
+									</td>
+								</tr>
+
+							</table>
+						</div>
+					</div>
+
+				</div>
+				<!-- END PAGE CONTENT-->
+			</div>
+		</div>
+		<!-- END CONTENT -->
+		<!-- BEGIN QUICK SIDEBAR -->
+		<!--Cooming Soon...-->
+		<!-- END QUICK SIDEBAR -->
+	</div>
+	<!-- END CONTAINER -->
+	<!-- BEGIN FOOTER -->
+	<div class="page-footer">
+		<div class="page-footer-inner">
+			 2014 &copy; Metronic by keenthemes. <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
+		</div>
+		<div class="scroll-to-top">
+			<i class="icon-arrow-up"></i>
+		</div>
+	</div>
+	<!-- END FOOTER -->
+</div>
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+
+<!--materialize JS-->
+<script src="materialize/js/materialize.js"></script>
+
+<script src="respond.min.js"></script>
+<script src="excanvas.min.js"></script> 
+<![endif]-->
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+<script src="js/jquery-ui.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="js/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="js/bootstrap-switch.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<script src="js/metronic.js" type="text/javascript"></script>
+<script src="js/layout.js" type="text/javascript"></script>
+<script src="js/demo.js" type="text/javascript"></script>
+<script>
+      jQuery(document).ready(function() {    
+         Metronic.init(); // init metronic core components
+Layout.init(); // init current layout
+Demo.init(); // init demo features
+      });
+   </script>
+<!-- END JAVASCRIPTS -->
+</body>
+<!-- END BODY -->
+</html>
