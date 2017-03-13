6c23ad06fee1be73f74c8b8f30d4a33ebe860021
Bereket <bereketgebredingle@gmail.com>
2017-03-13 12:13:19 +0300

    the table is changed to algorithm

--- resources/views/System_Analysis/Density_and_Deduct_value_view.blade.php
+++ resources/views/System_Analysis/Density_and_Deduct_value_view.blade.php
@@ -273,23 +273,109 @@
                                                     <td>{{$condition_index->Severity}}</td>
                                                     <td>{{$condition_index->Quantity}}</td>
                                                     <td>{{ $Density = $condition_index->Quantity / $Sum}}</td>
-                                                    <td>
+                                                    <td class="Deduct_values">
+                                                        {{--for faigue or alligatory--}}
                                                         @if($condition_index->Distress_type == "alligatory")
                                                             @if($condition_index->Severity == "low")
-                                                                {{  ( -0.0134 * pow($Density, 2) ) + (0.8889 * $Density) }}
+                                                                {{  10.749 + ( 15.867 * pow($Density, 1) ) + ( 7.6012 * pow($Density, 2) ) + ( -1.4052 * pow($Density, 3) ) }}
                                                             @elseif($condition_index->Severity == "medium")
-                                                                {{  ( 0.0001 * pow($Density, 3) ) - (0.0265 * pow($Density,2) ) + (1.5976 * $Density) }}
+                                                                {{  21.39 + ( 21.483 * pow($Density, 1) ) + ( 5.0615 * pow($Density, 2) ) + ( -1.5665 * pow($Density, 3) ) + ( 0.355 * pow($Density, 4) ) }}
                                                             @elseif($condition_index->Severity == "high")
-                                                                {{  ( 0.003 * pow($Density, 3) ) - (0.0534 * pow($Density,2) ) + (2.9935 * $Density) + 3.2856 }}
+                                                                {{  30.188 + ( 31.353 * pow($Density, 1) ) + ( 7.9737 * pow($Density, 2) ) + ( -10.227 * pow($Density, 3) ) + ( 0.2232 * pow($Density, 4) ) + ( 3.7003 * pow($Density, 5) ) + ( -1.1542 * pow($Density, 6) ) }}
                                                             @endif
+
+                                                            {{--bleeding--}}
                                                         @elseif($condition_index->Distress_type == "longtd_crack")
                                                             @if($condition_index->Severity == "low")
-                                                                {{  ( pow(4,-0.5) * pow($Density, 3) ) - (0.007 * pow($Density,2) ) + (0.7716 * pow($Density,2) ) + (0.4838 * $Density) + 0.7716 }}
+                                                                {{  1.8295 + ( -14.933 * pow($Density, 1) ) + ( 42.465 * pow($Density, 2) ) + ( -47.127 * pow($Density, 3) ) + ( 25.107 * pow($Density, 4) ) + ( -4.5804 * pow($Density, 5) ) }}
                                                             @elseif($condition_index->Severity == "medium")
-                                                                {{  ( 0.0001 * pow($Density, 3) ) - (0.0265 * pow($Density,2) ) + (1.5976 * $Density) }}
+                                                                {{  2.7872 + ( 5.3875 * pow($Density, 1) ) + ( 4.6612 * pow($Density, 2) ) + ( 0.3091 * pow($Density, 3) ) + ( -0.6957 * pow($Density, 4) ) + ( 0.5096 * pow($Density, 5) ) }}
                                                             @elseif($condition_index->Severity == "high")
-                                                                {{  ( 0.003 * pow($Density, 3) ) - (0.0534 * pow($Density,2) ) + (2.9935 * $Density) + 3.2856 }}
+                                                                {{  5.2119 + ( 6.414 * pow($Density, 1) ) + ( 7.4293 * pow($Density, 2) ) + ( 4.0615 * pow($Density, 3) ) + (  -0.4107 * pow($Density, 4) ) }}
                                                             @endif
+
+                                                            {{--block_cracking--}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  -0.1016 + ( 2.3786 * pow($Density, 1) ) + ( 8.6496 * pow($Density, 2) ) + ( -3.7548 * pow($Density, 3) ) + (  1.164 * pow($Density, 4) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  2.315 + ( 8.9552 * pow($Density, 1) ) + ( 5.602 * pow($Density, 2) ) + ( -3.4737 * pow($Density, 3) ) + ( 3.32 * pow($Density, 4) ) + ( -0.7577 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  6.0091 + ( 11.269 * pow($Density, 1) ) + ( 10.017 * pow($Density, 2) ) + ( 3.4588 * pow($Density, 3) ) + ( -1.5089 * pow($Density, 4) ) }}
+                                                            @endif
+
+                                                            {{--Edge cracking--}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  3.1444 + ( 2.0074 * pow($Density, 1) ) + ( 1.1778 * pow($Density, 2) ) + ( 6.222 * pow($Density, 3) ) + ( 2.0139 * pow($Density, 4) ) + ( -3.3278 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  8.2677 + ( 8.533 * pow($Density, 1) ) + ( 6.5905 * pow($Density, 2) ) + ( 1.8119 * pow($Density, 3) ) + ( -0.9679 * pow($Density, 4) ) + ( -1.349 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  13.367 + ( 13.955 * pow($Density, 1) ) + ( 12.973 * pow($Density, 2) ) + ( 6.5226 * pow($Density, 3) ) + ( -2.3835 * pow($Density, 4) ) + ( -4.1062 * pow($Density, 5) ) }}
+                                                            @endif
+
+                                                                {{--L & T cracking --}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  1.7349 + ( 6.0577 * pow($Density, 1) ) + ( 8.563 * pow($Density, 2) ) + ( 7.0654 * pow($Density, 3) ) + (  -11.37 * pow($Density, 4) ) + ( 4.3642 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  8.4355 + ( 14.045 * pow($Density, 1) ) + ( 5.2439 * pow($Density, 2) ) + ( 3.3775 * pow($Density, 3) ) + ( 2.1445 * pow($Density, 4) ) + ( -2.4006 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  17.67 + ( 22.303 * pow($Density, 1) ) + ( 15.702 * pow($Density, 2) ) + ( 11.802 * pow($Density, 3) ) + ( -0.432 * pow($Density, 4) ) + ( -4.7342 * pow($Density, 5) ) }}
+                                                            @endif
+
+                                                            {{--Patching --}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  2.1419 + ( 5.324 * pow($Density, 1) ) + ( 6.6383 * pow($Density, 2) ) + ( 5.2832 * pow($Density, 3) ) + ( -4.5093 * pow($Density, 4) ) + ( 1.0189 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  9.5535 + ( 12.007 * pow($Density, 1) ) + ( 6.5043 * pow($Density, 2) ) + ( 2.8351 * pow($Density, 3) ) + ( 0.9623 * pow($Density, 4) ) + ( -0.8932 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  19.016 + ( 16.806 * pow($Density, 1) ) + ( 3.9878 * pow($Density, 2) ) + ( 11.342 * pow($Density, 3) ) + ( 5.4961 * pow($Density, 4) ) + ( -5.7158 * pow($Density, 5) ) }}
+                                                            @endif
+
+                                                                {{-- Rutting --}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  8.0082 + ( 14.038 * pow($Density, 1) ) + ( 5.0636 * pow($Density, 2) ) + ( -0.0406 * pow($Density, 3) ) + (  1.4484 * pow($Density, 4) + ( -0.9035 * pow($Density, 5) ) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  17.663 + ( 19.717 * pow($Density, 1) ) + ( 7.8427 * pow($Density, 2) ) + ( 0.5225 * pow($Density, 3) ) + ( -1.5932 * pow($Density, 4) )  }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  26.761 + ( 23.525 * pow($Density, 1) ) + ( 9.4589 * pow($Density, 2) ) + ( 3.7395 * pow($Density, 3) ) + ( -3.2432 * pow($Density, 4) ) }}
+                                                            @endif
+
+                                                            {{--Potholes--}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  57.481 + ( 41.042 * pow($Density, 1) ) + ( 3.0305 * pow($Density, 2) ) + ( -1.5721 * pow($Density, 3) ) + ( 0.1291 * pow($Density, 4) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  90.65 + ( 66.661 * pow($Density, 1) ) + ( 7.8051 * pow($Density, 2) ) + ( -2.1575 * pow($Density, 3) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  109.11 + ( 58.957 * pow($Density, 1) ) + ( 1.3903 * pow($Density, 2) ) + ( -2.9872 * pow($Density, 3) ) }}
+                                                            @endif
+
+                                                                {{--Raveling --}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  1.7828 + ( 0.5165 * pow($Density, 1) ) + ( -0.6228 * pow($Density, 2) ) + ( 3.191 * pow($Density, 3) ) + (  0.9732 * pow($Density, 4) ) + (  -1.2907 * pow($Density, 5) + (  0.2628 * pow($Density, 6) ) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  8.4392 + ( 3.406 * pow($Density, 1) ) + ( 1.3728 * pow($Density, 2) ) + ( 5.739 * pow($Density, 3) ) + (  0.667 * pow($Density, 4) ) + (  -2.1711 * pow($Density, 5) + (  0.5652 * pow($Density, 6) ) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  15.741 + ( 9.3802 * pow($Density, 1) ) + ( 7.0157 * pow($Density, 2) ) + ( 15.47 * pow($Density, 3) ) + (  -0.3931 * pow($Density, 4) ) + (  -7.6863 * pow($Density, 5) + (  2.2487 * pow($Density, 6) ) ) }}
+                                                            @endif
+
+                                                            {{-- Shoving --}}
+                                                        @elseif($condition_index->Distress_type == "longtd_crack")
+                                                            @if($condition_index->Severity == "low")
+                                                                {{  3.8756 + ( 10.363 * pow($Density, 1) ) + ( 2.7931 * pow($Density, 2) ) + ( 5.7746 * pow($Density, 3) ) + ( -2.6249 * pow($Density, 4) ) + ( 0 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "medium")
+                                                                {{  9.4749 + ( 13.999 * pow($Density, 1) ) + ( 7.2303 * pow($Density, 2) ) + ( 4.1283 * pow($Density, 3) ) + ( 2.415 * pow($Density, 4) ) + ( -2.1604 * pow($Density, 5) ) }}
+                                                            @elseif($condition_index->Severity == "high")
+                                                                {{  18.608 + ( 16.77 * pow($Density, 1) ) + ( 12.338 * pow($Density, 2) ) + ( 8.1407 * pow($Density, 3) ) + ( -1.3562 * pow($Density, 4) ) + ( -2.3024 * pow($Density, 5) ) }}
+                                                            @endif
+
+                                                        @else
+                                                            {{$Density}}
 
                                                         @endif
                                                     </td>
@@ -307,11 +393,30 @@
                                 <div class="row">
 
                                     <div class="col-md-3">
-                                        <h5><b> Total Deduct Value:</b> lorem </h5>
+                                        <h5><b> Total Deduct Value:</b> <div class="inline" id="tdv">  </div> </h5>
                                     </div>
 
                                     <div class="col-md-3">
-                                        <h5><b> Total Deduct Value:</b> lorem </h5>
+                                        <h5><b> m: </b> <div class="inline" id="m">  </div> </h5>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <h5><b> Corrected Deduct value: </b> <div class="inline" id="cdv">  </div> </h5>
+                                    </div>
+
+                                </div>
+
+                                <div class="row ">
+                                    <div class="col-md-2">
+                                        <h4><b>PCI:</b></h4>
+                                    </div>
+                                    <div class="progress col-md-6" id="progress_bar_container">
+
+                                        <div id="Progress_bar" class="inline determinate" role="progressbar" aria-valuenow="40"
+                                             aria-valuemin="0" aria-valuemax="100" style="width:40%">
+                                            <br>
+                                        </div>
+
                                     </div>
 
                                 </div>
@@ -378,7 +483,7 @@
         Demo.init(); // init demo features
 
         $('#example').DataTable( {
-            "pageLength": 3,
+//            "pageLength": 3,
             columnDefs: [
                 {
                     targets: [ 0, 1, 2 ],
@@ -428,6 +533,85 @@
         });
 
         $('#example_filter').appendTo('#append');
+
+        var valuesArray = new Array();
+
+        $('.Deduct_values').each(function () {
+            valuesArray.push( Number ($(this)[0].innerText ));
+        });
+
+        console.log( valuesArray );
+        var max_Dv = Math.max.apply(Math, valuesArray);
+        m = 1 + ( (9/98) * (100 - max_Dv ) );
+        console.log(m);
+        $('#m').html(Math.round(m * 1000) / 1000);
+
+        var sum = 0;
+        $.each(valuesArray,function(){sum+=parseFloat(this) || 0;});
+        console.log(sum);
+        $('#tdv').html(Math.round(sum * 1000) / 1000);
+
+        // Cdv is to be calculated here
+
+        var Cdv =  90;
+
+        var pci = 100 - Cdv;
+//        $('#pci').html(pci);
+
+        if (85 < pci && pci < 100)
+        {
+            // Good Dark green
+            $('#Progress_bar').css('background-color', "#006400" );
+            $('#Progress_bar').css('color', "#fff" );
+            $('#Progress_bar').html(  pci + "% " + "Good");
+            $('#Progress_bar').css('width', pci + "%" );
+
+        }
+        else if( pci > 70)
+        {
+
+            $('#Progress_bar').css('background-color', "#90EE90" );
+            $('#Progress_bar').css('color', "#000" );
+            $('#Progress_bar').html(  pci + "% " + "Satisfactory");
+            $('#Progress_bar').css('width', pci + "%" );
+
+        }
+        else if(pci > 55)
+        {
+            $('#Progress_bar').css('background-color', "#ffff00" );
+            $('#Progress_bar').css('color', "#000" );
+            $('#Progress_bar').html(  pci + "% " + "Fair");
+            $('#Progress_bar').css('width', pci + "%" );
+        }
+        else if(pci > 40)
+        {
+            $('#Progress_bar').css('background-color', "#ff7777" );
+            $('#Progress_bar').css('color', "#000" );
+            $('#Progress_bar').html(  pci + "% " + "Poor");
+            $('#Progress_bar').css('width', pci + "%" );
+        }
+        else if(pci > 25)
+        {
+            $('#Progress_bar').css('background-color', "#ff5454" );
+            $('#Progress_bar').css('color', "#fff" );
+            $('#Progress_bar').html(  pci + "% " + "Very Poor");
+            $('#Progress_bar').css('width', pci + "%" );
+        }
+        else if(pci > 10)
+        {
+            $('#Progress_bar').css('background-color', "#ff0000" );
+            $('#Progress_bar').css('color', "#fff" );
+            $('#Progress_bar').html(  pci + "% " + "Serious");
+            $('#Progress_bar').css('width', pci + "%" );
+        }
+        else
+        {
+            $('#Progress_bar').hide();
+            $('#progress_bar_container').removeClass('progress')
+            $('#progress_bar_container').html( "<h4> Failed </h4>" );
+        }
+
+
     });
 </script>
 <!-- END JAVASCRIPTS -->
